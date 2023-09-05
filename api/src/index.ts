516dff53ffafaf6e7837e0ac78c43040636de2ad
M Gilang Januar <mgilangjanuar@gmail.com>
2023-09-05 17:37:41 +0700

    update deps

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,1 +1,10 @@
-console.log('hello')
+import cors from 'cors'
+import express from 'express'
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.listen(4000, () => {
+  console.log('Server listening on port 4000')
+})
